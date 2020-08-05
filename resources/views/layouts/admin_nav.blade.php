e0e1022e47792323f9e722327f72f030772cc84b
dudj <13020078873@163.com>
2020-08-05 15:27:05 +0800

    修改顶部导航

--- resources/views/layouts/admin_nav.blade.php
+++ resources/views/layouts/admin_nav.blade.php
@@ -2,34 +2,12 @@
     <nav class="navbar navbar-default navbar-static-top">
         <!-- 顶部开始 -->
         <div class="container">
-            <div class="logo">
-                <a href="{{ url('/') }}">管理中心</a></div>
-            <div class="left_open">
-                <a><i title="展开左侧栏" class="iconfont">&#xe699;</i></a>
+            <div class="logo" data-status = '1'>
+                <a href="{{ url('/') }}">管理中心</a>
             </div>
-            <ul class="layui-nav left fast-add" lay-filter="">
-                <li class="layui-nav-item">
-                    <a href="javascript:;">+新增</a>
-                    <dl class="layui-nav-child">
-                        <!-- 二级菜单 -->
-                        <dd>
-                            <a onclick="xadmin.open('最大化','http://www.baidu.com','','',true)">
-                                <i class="iconfont">&#xe6a2;</i>弹出最大化</a></dd>
-                        <dd>
-                            <a onclick="xadmin.open('弹出自动宽高','http://www.baidu.com')">
-                                <i class="iconfont">&#xe6a8;</i>弹出自动宽高</a></dd>
-                        <dd>
-                            <a onclick="xadmin.open('弹出指定宽高','http://www.baidu.com',500,300)">
-                                <i class="iconfont">&#xe6a8;</i>弹出指定宽高</a></dd>
-                        <dd>
-                            <a onclick="xadmin.add_tab('在tab打开','member-list.html')">
-                                <i class="iconfont">&#xe6b8;</i>在tab打开</a></dd>
-                        <dd>
-                            <a onclick="xadmin.add_tab('在tab打开刷新','member-del.html',true)">
-                                <i class="iconfont">&#xe6b8;</i>在tab打开刷新</a></dd>
-                    </dl>
-                </li>
-            </ul>
+            <div class="left_open">
+                <a class="leftOperate"><i title="展开左侧栏" class="iconfont">&#xe699;</i></a>
+            </div>
             <div class="layui-fluid" id="updatePwd" style="display: none">
                 <div class="layui-row">
                     <div class="layui-form-item">
@@ -118,5 +96,15 @@
                 });
             }
         }
+        $('.leftOperate').click(function(){
+            var status = $('.logo').attr('data-status');
+            if(status > 0){
+                $('.logo').css('display','none');
+                $('.logo').attr('data-status',0);
+            }else{
+                $('.logo').css('display','block');
+                $('.logo').attr('data-status',1);
+            }
+        })
     </script>
 @endsection
